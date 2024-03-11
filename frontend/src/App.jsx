c003e8cd417a449aa7071ea6a4bd131c53ceb117
nimit sodhani <nimitsodhani1@gmail.com>
2024-03-12 02:36:30 +0530

    working on frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {CreateTodo} from './components/CreateTodo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>  
-      hi there    
+    <div>
+        < CreateTodo></CreateTodo>
     </div>
   )
 }
