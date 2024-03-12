415dbb6618d815287c24dc619dafd94a008979f6
nimit sodhani <nimitsodhani1@gmail.com>
2024-03-12 20:48:45 +0530

    todos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {CreateTodo} from './components/CreateTodo'
+import {Todos} from './components/Todos'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <div>
         < CreateTodo></CreateTodo>
+      <Todos></Todos>
     </div>
   )
 }
--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,3 +1,8 @@
 export function Todos () {
     
+    return <div>
+            <h1> go to gym  </h1>
+            <h2> yo need to go </h2>
+            <button> mark as completed </button>
+    </div>
 }
